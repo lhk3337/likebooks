e09be243efe5efff968411b809445a9801316b7f
HK <lhk3337@naver.com>
2020-03-26 22:22:39 +0900

    add more controller

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -19,14 +19,14 @@ export const postJoin = (req, res) => {
 export const getLogin = (req, res) =>
   res.render("globalPug/login", { pageTitle: "LOG IN" });
 
-export const postLogin = (req, res) => res.redirect(route.home);
-
-export const logout = (req, res) =>
-  res.render("globalPug/logout", { pageTitle: "LOGOUT" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+export const logout = (req, res) => res.redirect(routes.home);
 
 export const userDetail = (req, res) =>
-  res.render("userPug/userDetail", { pageTitle: "USERDETAIL" });
+  res.render("userPug/userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
-  res.render("userPug/editProfile", { pageTitle: "EDITPROFILE" });
+  res.render("userPug/editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
-  res.render("userpug/changePassword", { pageTitle: "CHANGEPASSWORD" });
+  res.render("userpug/changePassword", { pageTitle: "Change Password" });
--- server/controller/videoController.js
+++ server/controller/videoController.js
@@ -1,5 +1,5 @@
 import { videos } from "../db";
-
+import routes from "../router/routes";
 export const home = (req, res) =>
   res.render("globalPug/home", { pageTitle: "HOME", videos });
 export const search = (req, res) => {
@@ -9,8 +9,15 @@ export const search = (req, res) => {
   console.log(searchingBy);
   res.render("globalPug/search", { pageTitle: "SEARCH", searchingBy, videos });
 };
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("videoPug/upload", { pageTitle: "UPLOAD" });
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  res.redirect(routes.videoDetail(344392));
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoPug/videoDetail", { pageTitle: "VIDEODETAIL" });
 export const editVideo = (req, res) =>
